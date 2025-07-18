68fe3ba23d7a7e3448db882df6e5255be0a8f985
mariawanjiru <rushi.mariah@gmail.com>
2025-07-18 13:57:34 +0300

    phase-0-javascript-events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  const left = parseInt(dodger.style.left.replace("px", ""), 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const left = parseInt(dodger.style.left.replace("px", ""), 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
